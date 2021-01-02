30c3ec463412085f5a59d8f900cbcfafcf87c1f4
KetanDev26 <sonik5097@gmail.com>
2021-01-02 17:04:34 +0530

    commit

--- setupProxy.js
+++ setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/Demo',
+    '/Demo/**',
     createProxyMiddleware({
       target: 'http://localhost:5000',
       changeOrigin: true,
